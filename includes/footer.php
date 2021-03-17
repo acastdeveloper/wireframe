31f544fffc9a632eeb6f420b900925d1d93127e0
Àlex Castelló <ac.desenvolupador@gmail.com>
2021-03-17 02:25:36 +0100

    End of speed tests by minizing files. It doesn't get a better performance

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,27 @@
-<footer>© 2021{ac} alexandercastello.com | All rights reserved | <a href="#">Contact</a></footer></div><script type="text/javascript">WebFontConfig={ google:{ families: ['Noto+Sans+JP:100,300,400,500,700', 'Nunito+Sans:200,300,400,600', 'Oswald:200,300,400,600,700', 'Roboto+Slab:200,400,600' ]}}; (function (){ var wf=document.createElement('script'); wf.src=('https:'==document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js'; wf.type='text/javascript'; wf.async='true'; var s=document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wf, s)})(); </script><script src="https://kit.fontawesome.com/f5043cec08.js" crossorigin="anonymous"></script><script type="text/javascript">document.getElementById("wrap").style.display="grid"; </script></body></html>
+<footer>© 2021{ac} alexandercastello.com | All rights reserved | <a href="#">Contact</a></footer>
+</div>
+<script type="text/javascript">
+    WebFontConfig = {
+        google: {
+            families: ['Noto+Sans+JP:100,300,400,500,700', 'Nunito+Sans:200,300,400,600',
+                'Oswald:200,300,400,600,700', 'Roboto+Slab:200,400,600'
+            ]
+        }
+    };
+    (function () {
+        var wf = document.createElement('script');
+        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+            '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s)
+    })();
+</script>
+<script src="https://kit.fontawesome.com/f5043cec08.js" crossorigin="anonymous"></script>
+<script type="text/javascript">
+    document.getElementById("wrap").style.display = "grid";
+</script>
+</body>
+
+</html>
