b0c9cd8b8a18b7f32c208575ac168129fd742e8a
Àlex Castelló <ac.desenvolupador@gmail.com>
2021-03-18 12:29:36 +0100

    Finally solved isue of CSS-GRID for the php version of index.php

--- includes/header.php
+++ includes/header.php
@@ -30,10 +30,6 @@
 </head>
 
 <body>
-    <?php
-        echo($_SERVER["SCRIPT_NAME"]);
-    ?>
-
     <div id="wrap">
         <header>
             <h1 id="logo"><a href="./" alt="home">{ac} alexander castelló / </a><span class="tag midcolor">Code &
