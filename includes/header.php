4b2d1d111bb2a4cbca91edbd9a2df32fa8a19a5c
Àlex Castelló <ac.desenvolupador@gmail.com>
2021-03-20 22:22:00 +0100

    test

--- includes/header.php
+++ includes/header.php
@@ -20,7 +20,7 @@
 
 
     <link rel="preconnect" href="https://kit.fontawesome.com">
-    <link rel="preload" href="https://kit.fontawesome.com/f5043cec08.js" crossorigin="anonymous" as="script" type="text/javascript">
+    <link rel="preload" href="https://kit.fontawesome.com/f5043cec08.js" crossorigin="anonymous" as="script" type="text/javascript" async>
     <!-- <link rel="preload" href="https://ka-f.fontawesome.com/releases/v5.15.2/css/free.min.css?token=f5043cec08" as="style" crossorigin="anonymous" type="text/css"> -->
 
 
