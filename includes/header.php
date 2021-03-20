049dfb2cabd89d892a3b351898e8b726563c26d7
Àlex Castelló <ac.desenvolupador@gmail.com>
2021-03-20 13:13:41 +0100

    Experiment of preloading external fonts in fontawesome and googlefonts to improve the performance

--- includes/header.php
+++ includes/header.php
@@ -15,13 +15,24 @@
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
     <link rel="preconnect" href="https://fonts.gstatic.com" />
-    <link rel="preload" href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:100,300,400,500,700%7CNunito+Sans:200,300,400,600%7COswald:200,300,400,600,700%7CRoboto+Slab:200,400,600" as="style">
+    <link rel="preload"
+        href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:100,300,400,500,700%7CNunito+Sans:200,300,400,600%7COswald:200,300,400,600,700%7CRoboto+Slab:200,400,600"
+        as="style">
     <link rel="preconnect" href="https://kit.fontawesome.com" />
     <link rel="stylesheet" href="./css/2/style2.css">
     <link rel="stylesheet" href="./css/2/estructura2-min.css">
     <link rel="stylesheet" href="./css/2/media2-min.css">
     <link rel="preload" href="imgs/bg/vecto-bg.svg" as="image">
-      <link rel="preload" href="https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js" as="script">
+    <link rel="preload" href="https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js" as="script">
+    <link rel="preload" as="font" href="https://ka-f.fontawesome.com/releases/v5.15.2/webfonts/free-fa-solid-900.woff2">
+    <link rel="preload" as="font" href="https://ka-f.fontawesome.com/releases/v5.15.2/webfonts/free-fa-brands-400.woff2">
+    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/oswald/v36/TK3iWkUHHAIjg752GT8G.woff2">
+    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/notosansjp/v28/-F62fjtqLzI2JPCgQBnw7HFow2oe2EcP5pp0erwTqsSWs9Jezazjcb4.119.woff2">
+    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/notosansjp/v28/-F62fjtqLzI2JPCgQBnw7HFow2oe2EcP5pp0erwTqsSWs9Jezazjcb4.118.woff2">
+    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/nunitosans/v6/pe03MImSLYBIv1o4X1M8cc9iB85tU1E.woff2">
+    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/robotoslab/v13/BngMUXZYTXPIvIBgJJSb6ufN5qU.woff2">
+
+
 
     <style>
         #wrap {
