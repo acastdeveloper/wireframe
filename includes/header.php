435f3f5e4546c4c64649062e344fb42d0a925235
Àlex Castelló <ac.desenvolupador@gmail.com>
2021-03-20 13:55:46 +0100

    Another experiment of preloading external fonts to improve performance

--- includes/header.php
+++ includes/header.php
@@ -16,25 +16,48 @@
     <meta name="theme-color" content="#ffffff">
 
     <link rel="preconnect" href="https://fonts.gstatic.com" />
-    <!-- PRELOADING GOOGLE FONTS AS FONTS -->
+    <!-- PRELOADING GOOGLEFONTS FILES -->
     <link rel="preload" as="font" href="https://fonts.gstatic.com/s/oswald/v36/TK3iWkUHHAIjg752GT8G.woff2" crossorigin>
-    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/notosansjp/v28/-F62fjtqLzI2JPCgQBnw7HFow2oe2EcP5pp0erwTqsSWs9Jezazjcb4.119.woff2" crossorigin>
-    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/notosansjp/v28/-F62fjtqLzI2JPCgQBnw7HFow2oe2EcP5pp0erwTqsSWs9Jezazjcb4.118.woff2" crossorigin>
-    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/nunitosans/v6/pe03MImSLYBIv1o4X1M8cc9iB85tU1E.woff2" crossorigin>
-    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/robotoslab/v13/BngMUXZYTXPIvIBgJJSb6ufN5qU.woff2" crossorigin>
-    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/notosansjp/v28/-F6ofjtqLzI2JPCgQBnw7HFQoggPkENvl4B0ZLgOquiXidBa3qHiDcp2RQ.119.woff2" crossorigin>
-    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/notosansjp/v28/-F6pfjtqLzI2JPCgQBnw7HFQaioq1xVxjfp_dakBof6Bs-tb3ab2FNISVac.119.woff2" crossorigin>
-    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/nunitosans/v6/pe03MImSLYBIv1o4X1M8cc9yAs5tU1E.woff2" crossorigin>
-    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/nunitosans/v6/pe03MImSLYBIv1o4X1M8cc8WAc5tU1E.woff2" crossorigin>
-    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/nunitosans/v6/pe0qMImSLYBIv1o4X1M8cce9I9s.woff2" crossorigin>
+    <link rel="preload" as="font"
+        href="https://fonts.gstatic.com/s/notosansjp/v28/-F62fjtqLzI2JPCgQBnw7HFow2oe2EcP5pp0erwTqsSWs9Jezazjcb4.119.woff2"
+        crossorigin>
+    <link rel="preload" as="font"
+        href="https://fonts.gstatic.com/s/notosansjp/v28/-F62fjtqLzI2JPCgQBnw7HFow2oe2EcP5pp0erwTqsSWs9Jezazjcb4.118.woff2"
+        crossorigin>
+    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/nunitosans/v6/pe03MImSLYBIv1o4X1M8cc9iB85tU1E.woff2"
+        crossorigin>
+    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/robotoslab/v13/BngMUXZYTXPIvIBgJJSb6ufN5qU.woff2"
+        crossorigin>
+    <link rel="preload" as="font"
+        href="https://fonts.gstatic.com/s/notosansjp/v28/-F6ofjtqLzI2JPCgQBnw7HFQoggPkENvl4B0ZLgOquiXidBa3qHiDcp2RQ.119.woff2"
+        crossorigin>
+    <link rel="preload" as="font"
+        href="https://fonts.gstatic.com/s/notosansjp/v28/-F6pfjtqLzI2JPCgQBnw7HFQaioq1xVxjfp_dakBof6Bs-tb3ab2FNISVac.119.woff2"
+        crossorigin>
+    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/nunitosans/v6/pe03MImSLYBIv1o4X1M8cc9yAs5tU1E.woff2"
+        crossorigin>
+    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/nunitosans/v6/pe03MImSLYBIv1o4X1M8cc8WAc5tU1E.woff2"
+        crossorigin>
+    <link rel="preload" as="font" href="https://fonts.gstatic.com/s/nunitosans/v6/pe0qMImSLYBIv1o4X1M8cce9I9s.woff2"
+        crossorigin>
+    <link rel="preload" href="https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js" as="script" crossorigin>
+    <link rel="preload" href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:100,300,400,500,700%7CNunito+Sans:200,300,400,600%7COswald:200,300,400,600,700%7CRoboto+Slab:200,400,600" as="style" crossorigin="">
+
+
+
+
 
 
     <link rel="preconnect" href="https://kit.fontawesome.com" />
-    <!-- PRELOADING FONTAWESOME FONTS AS FONTS -->
+    <!-- PRELOADING FONTAWESOME FILES -->
     <link rel="preload" as="font" href="https://ka-f.fontawesome.com/releases/v5.15.2/webfonts/free-fa-solid-900.woff2"
         crossorigin>
     <link rel="preload" as="font" href="https://ka-f.fontawesome.com/releases/v5.15.2/webfonts/free-fa-brands-400.woff2"
         crossorigin>
+    <link rel="preload" href="https://ka-f.fontawesome.com/releases/v5.15.2/css/free.min.css?token=f5043cec08"
+        as="style" crossorigin>
+    <link rel="preload" href="https://ka-f.fontawesome.com/releases/v5.15.2/css/free-v4-shims.min.css?token=f5043cec08" as="style" crossorigin>
+    <link rel="preload" href="https://ka-f.fontawesome.com/releases/v5.15.2/css/free-v4-font-face.min.css?token=f5043cec08" as="style" crossorigin>
 
 
     <link rel="preload" href="imgs/bg/vecto-bg.svg" as="image">
