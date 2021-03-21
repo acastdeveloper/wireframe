f61aad3c64c3b47f18d051312530b83051314d50
Àlex Castelló <ac.desenvolupador@gmail.com>
2021-03-21 12:47:40 +0100

    Fix

--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,7 @@
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" async>
 
     <!-- preload style2 -->
-    <link async rel="preload"  as="style" type="text/css" href="./css/2/style2-min.css" onload="this.rel='stylesheets'">
+    <link async rel="preload"  as="style" type="text/css" href="./css/2/style2-min.css" onload="this.rel='stylesheet'">
 
     <!-- GOOGLE FONTS -->
     <link rel="preconnect" href="https://fonts.gstatic.com" />
